8cf6dc267f4c17705f290976c2cef18cb7b1099f
danleoparra <danpato.25@gmail.com>
2026-03-18 10:39:51 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import TennisReachVisualizer from "./TennisReachVisualizer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <TennisReachVisualizer />
   </React.StrictMode>
 );
